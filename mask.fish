0c52b83981cca8f777541d0a879eb1e6e32aba80
Yuriko <yuriko.nishijima@gmail.com>
2025-03-31 17:51:33 +0100

    update fish script

---
+++ mask.fish
@@ -0,0 +1,26 @@
+#!/usr/bin/env fish
+
+osascript -e '
+tell application "iTerm"
+    activate
+    create window with default profile
+
+    tell current window
+        -- Pane 1: client1
+        tell current session
+            write text "cd ./vfl-demo/clients/client1/client1_masking && fish client1_mask.fish"
+            split vertically with default profile
+        end tell
+
+        -- Pane 2: client2
+        tell session 2 of current tab
+            write text "cd ./vfl-demo/clients/client2/client2_masking && fish client2_mask.fish"
+            split vertically with default profile
+        end tell
+
+        -- Pane 3: client3
+        tell session 3 of current tab
+            write text "cd ./vfl-demo/clients/client3/client3_masking && fish client3_mask.fish"
+        end tell
+    end tell
+end tell'
---
+++ train.fish
@@ -0,0 +1,26 @@
+#!/usr/bin/env fish
+
+osascript -e '
+tell application "iTerm"
+    activate
+    create window with default profile
+
+    tell current window
+        -- Pane 1: client1
+        tell current session
+            write text "cd ./vfl-demo/clients/client1/client1_training && fish client1_train.fish"
+            split vertically with default profile
+        end tell
+
+        -- Pane 2: client2
+        tell session 2 of current tab
+            write text "cd ./vfl-demo/clients/client2/client2_training && fish client2_train.fish"
+            split vertically with default profile
+        end tell
+
+        -- Pane 3: client3
+        tell session 3 of current tab
+            write text "cd ./vfl-demo/clients/client3/client3_training && fish client3_train.fish"
+        end tell
+    end tell
+end tell'
