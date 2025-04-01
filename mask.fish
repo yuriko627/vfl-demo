2c32d3538a051b59064175e6cd46827d7d5fced9
Yuriko <yuriko.nishijima@gmail.com>
2025-04-01 18:38:28 +0100

    client -> Client, fix typo

--- mask.fish
+++ mask.fish
@@ -6,19 +6,19 @@ tell application "iTerm"
     create window with default profile
 
     tell current window
-        -- Pane 1: client1
+        -- Pane 1: Client1
         tell current session
             write text "cd ./vfl-demo/clients/client1/client1_masking && fish client1_mask.fish"
             split vertically with default profile
         end tell
 
-        -- Pane 2: client2
+        -- Pane 2: Client2
         tell session 2 of current tab
             write text "cd ./vfl-demo/clients/client2/client2_masking && fish client2_mask.fish"
             split vertically with default profile
         end tell
 
-        -- Pane 3: client3
+        -- Pane 3: Client3
         tell session 3 of current tab
             write text "cd ./vfl-demo/clients/client3/client3_masking && fish client3_mask.fish"
         end tell
--- train.fish
+++ train.fish
@@ -6,19 +6,19 @@ tell application "iTerm"
     create window with default profile
 
     tell current window
-        -- Pane 1: client1
+        -- Pane 1: Client1
         tell current session
             write text "cd ./vfl-demo/clients/client1/client1_training && fish client1_train.fish"
             split vertically with default profile
         end tell
 
-        -- Pane 2: client2
+        -- Pane 2: Client2
         tell session 2 of current tab
             write text "cd ./vfl-demo/clients/client2/client2_training && fish client2_train.fish"
             split vertically with default profile
         end tell
 
-        -- Pane 3: client3
+        -- Pane 3: Client3
         tell session 3 of current tab
             write text "cd ./vfl-demo/clients/client3/client3_training && fish client3_train.fish"
         end tell
